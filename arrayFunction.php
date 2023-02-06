9f206247a6dc4edf225a1125135726a08fb84520
Saugat Poudel <njsaugat@gmail.com>
2023-02-06 09:07:21 +0545

    array functions added

--- arrayFunction.php
+++ arrayFunction.php
@@ -4,3 +4,8 @@ $numbers=[1,2,4,5,6];
 $numbersModified=array_map(function ($num){
     return "Number is ${num}";
 },$numbers);
+
+$numberLT5=array_filter($numbers, function($number){
+    return $number<5;
+});
+
