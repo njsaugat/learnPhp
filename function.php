3183c059d325e0b7b6c15fd6144699265da7f3d4
Saugat Poudel <njsaugat@gmail.com>
2023-02-12 08:02:41 +0545

    function file added

--- function.php
+++ function.php
@@ -3,4 +3,7 @@
     $numbersModified=array_map(function ($num){
         return "Number is ${num}";
     },$names);
-    
+
+    $numberLT5=array_filter($numbers, function($number){
+        return $number>5;
+    });
